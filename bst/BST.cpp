c113fa8bb816986ed728898571205993f8793d81
Tatiana Retiukhina <taretiuk@student.42berlin.de>
2025-07-07 16:43:11 +0200

    feat(bst): work in progress

---
+++ bst/BST.cpp
@@ -0,0 +1,65 @@
+#include "BST.hpp"
+#include <cstdlib>
+
+BST::BST() {
+    root = NULL;
+}
+
+BST::node* BST::createLeaf(int key) {
+    node* n = new node;
+    n->key = key;
+    n->left = n->right = NULL;
+    return n;
+}
+
+void BST::addLeaf(int key) {
+    addLeafPrivate(key, root);
+}
+
+void BST::addLeafPrivate(int key, node* Ptr) {
+    if(root == NULL) {
+        root = createLeaf(key);
+    }
+    else if (key < Ptr->key) {
+        if (Ptr->left != NULL) {
+            addLeafPrivate(key, Ptr->left);
+        }
+        else {
+            Ptr->left = createLeaf(key);
+        }
+    }
+    else if (key > Ptr->key) {
+        if (Ptr->right != NULL) {
+            addLeafPrivate(key, Ptr->right);
+        }
+        else {
+            Ptr->right = createLeaf(key);
+        }
+    }
+    else {
+        cout << "The key " << key << " has been added to the tree\n";
+    }
+}
+
+void BST::printInOrder() {
+    printInOrderPrivate(root);
+}
+
+// in-order traversal
+// 1. go left
+// 2. process current node
+// 3. go right
+void BST::printInOrderPrivate(node* Ptr) {
+    if (root != NULL) {
+        if (Ptr->left != NULL) {
+            printInOrderPrivate(Ptr->left);
+        }
+        cout << Ptr->key << " ";
+        if (Ptr->right != NULL) {
+            printInOrderPrivate(Ptr->left);
+        }
+    }
+    else {
+        cout << "The tree is empty!\n";
+    }
+}
---
+++ bst/main.cpp
@@ -0,0 +1,22 @@
+#include "BST.hpp"
+#include <cstdlib>
+
+// in-order traversal
+// 1. go left
+// 2. process current node
+// 3. go right
+
+int main() {
+    int treeKeys[16] = { 50, 76, 21, 4, 32, 64, 15, 52, 14, 100, 83, 2, 3, 70, 87, 80 };
+    BST myTree;
+
+    cout << "Printing the tree before adding numbers\n";
+    myTree.printInOrder();
+
+    cout << "Printing the tree after adding numbers\n";
+    for (int i = 0; i < 16; i++) {
+        myTree.addLeaf(treeKeys[i]);
+    }
+    myTree.printInOrder();
+}
+
