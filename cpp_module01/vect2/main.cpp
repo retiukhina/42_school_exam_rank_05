f8f0941036753a4bb78e4578ca8b82e18ab8b6bb
Tatiana <taretiuk@student.42berlin.de>
2025-07-08 14:31:46 +0200

    fixup! feat(vect2): add constructors, stdout operator overload function

---
+++ cpp_module01/vect2/main.cpp
@@ -0,0 +1,34 @@
+#include "vect2.hpp"
+
+int main() {
+    vect2 v1; // 0, 0
+    vect2 v2(1, 2); // 1, 2
+    const vect2 v3(v2); 
+    vect2 v4 = v2;
+    cout << "v1: " << v1 << endl;
+    //cout << "v1: " << "{" << v1[0] << ", " << v1[1] << "}" << endl; // the same output
+    cout << "v2: " << v2 << endl;
+    cout << "v3: " << v3 << endl;
+    cout << "v4: " << v4 << endl;
+    // cout << v4++ << endl;
+    // cout << ++v4 << endl;
+    // cout << v4-- << endl;
+    // cout << --v4 << endl;
+    // v2 += v3;
+    // v1 -= v2;
+    // v2 = v3 + v3 * 2;
+    // v2 = 3 * v2;
+    // v2 += v2 += v3;
+    // v1 *= 42;
+    // v1 = v1 - v1 + v1;
+    cout << "v1: " << v1 << endl;
+    cout << "v2: " << v2 << endl;
+    // cout << "-v2: " << -v2 << endl;
+    // cout << "v1[1]: " << v1[1] << endl;
+    // v1[1] = 12;
+    // cout << "v1[1]: " << v1[1] << endl;
+    // cout << "v3[1]: " << v3[1] << endl;
+    cout << "v1 == v1 " << (v1 == v1) << endl;
+    cout << "v1 != v3 " << (v1 != v3) << endl;
+}
+
---
+++ cpp_module01/vect2/vect2.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <ostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std:: ostream;
+
+class vect2 {
+    private:
+        int _x;
+        int _y;
+        int _arr[2];
+    
+    public:
+        vect2();
+        vect2(const int x, const int y);
+        vect2(const vect2& other);
+        vect2& operator=(const vect2& other);
+        ~vect2();
+
+        void createVector();
+        void setInts(const int x, const int y);
+        void printVector() const;
+
+        bool operator==(const vect2& other) const;
+        bool operator!=(const vect2& other) const;
+
+
+        friend std::ostream& operator<<(std::ostream& os, const vect2& v);
+};
